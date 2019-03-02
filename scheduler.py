8704dd658afa6ad8d1d994eeb3cd7070d05928a3
Pranav <m_pranav@yahoo.com>
2019-03-02 12:05:31 -0600

    test scheduler for deployment

--- scheduler.py
+++ scheduler.py
@@ -4,8 +4,8 @@ import time
 from App_Setup import app as setupapp
 from App_batch_update import app as updateapp
 
-setupapp()
-updateapp()
+# setupapp()
+# updateapp()
 
 schedule.every().day.at("8:35:00").do(updateapp)
 
