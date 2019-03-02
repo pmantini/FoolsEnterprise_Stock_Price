e99cfb1283481db8e6f6818ff7aa66758c9794fe
Pranav <m_pranav@yahoo.com>
2019-03-02 12:37:16 -0600

    Updated scheduler

--- scheduler.py
+++ scheduler.py
@@ -1,18 +1,13 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
-import time
 
 from App_Setup import app as setupapp
 from App_batch_update import app as updateapp
 
-# setupapp()
-# updateapp()
+setupapp()
+updateapp()
 
 sched = BlockingScheduler()
 
 sched.scheduled_job(updateapp, 'cron', hour=8, minute=40)
+sched.start()
 
-# schedule.every().day.at("8:35:00").do(updateapp)
-#
-# while True:
-#     schedule.run_pending()
-#     time.sleep(60) # wait one minute
