511432cec15b70b0bd4fbca8d1ba614108a1615e
Pranav <m_pranav@yahoo.com>
2019-03-02 12:33:33 -0600

    Replaced schedule with appschduler

--- scheduler.py
+++ scheduler.py
@@ -1,4 +1,4 @@
-import schedule
+from apscheduler.schedulers.blocking import BlockingScheduler
 import time
 
 from App_Setup import app as setupapp
@@ -7,8 +7,12 @@ from App_batch_update import app as updateapp
 # setupapp()
 # updateapp()
 
-schedule.every().day.at("8:35:00").do(updateapp)
+sched = BlockingScheduler()
 
-while True:
-    schedule.run_pending()
-    time.sleep(60) # wait one minute
+sched.scheduled_job(updateapp, 'cron', hour=8, minute=40)
+
+# schedule.every().day.at("8:35:00").do(updateapp)
+#
+# while True:
+#     schedule.run_pending()
+#     time.sleep(60) # wait one minute
