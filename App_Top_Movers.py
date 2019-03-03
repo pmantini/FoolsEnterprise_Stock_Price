ad4574496eeca5a7191e55df6f2ac2d3d50795dd
Pranav <m_pranav@yahoo.com>
2019-03-03 08:53:12 -0600

    Updated top movers app to delete row from Db

--- App_Top_Movers.py
+++ App_Top_Movers.py
@@ -38,9 +38,15 @@ def app():
 
     i = 0
 
+    db_name = "change.db"
+    changed_db = sqlite3.connect(db_folder + '/' + db_name)
+    cursor = changed_db.cursor()
+    table_name = "data"
+
     for k in list_of_stocks:
         if k[0] in blacklist:
             logger.info("%s is Blacklisted - popping from list" % (k[0]))
+            cursor.execute("DELETE FROM %s WHERE stock_symbol=\'%s\'" % (table_name, k[0]))
             list_of_stocks.pop(i)
             i += 1
 
@@ -59,10 +65,7 @@ def app():
 
 
 
-    db_name = "change.db"
-    changed_db = sqlite3.connect(db_folder+'/'+db_name)
-    cursor = changed_db.cursor()
-    table_name = "data"
+
 
     cursor.execute("create table if not exists %s (stock_symbol TEXT, stock_change TEXT, stock_change_percentage TEXT, CONSTRAINT stock_name_unique UNIQUE (stock_symbol))" % (table_name))
 
