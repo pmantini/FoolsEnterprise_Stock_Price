f758582d038e3c27f1d033f9704dc2636f947489
pmantini <m_pranav@yahoo.com>
2019-03-05 22:07:30 -0600

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -3,11 +3,11 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from App_Setup import app as setupapp
 from App_batch_update import app as updateapp
 
-setupapp()
+#setupapp()
 updateapp()
 
 sched = BlockingScheduler()
 
-sched.scheduled_job(updateapp, 'cron', hour=15, minute=00)
+sched.scheduled_job(updateapp, 'cron', hour=9, minute=00)
 sched.start()
 
