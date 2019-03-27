763493836bb2bd8304e975788f2b776e0746d122
pmantini <m_pranav@yahoo.com>
2019-03-27 09:10:36 -0500

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -3,7 +3,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from App_Setup import app as setupapp
 from App_batch_update import app as updateapp
 
-#setupapp()
+setupapp()
 updateapp()
 
 sched = BlockingScheduler()
