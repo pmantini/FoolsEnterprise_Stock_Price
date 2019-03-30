215e42103b9b3437601553c9ae23f3f99b8a740c
Pranav <m_pranav@yahoo.com>
2019-03-30 03:19:16 -0500

    v2 start fresh

--- Hyper_Setup.py
+++
@@ -1,5 +0,0 @@
-db_folder = 'Databases'
-start_date = "2008-01-01"
-log_file_name_Setup = "fools-enterprise-log_Setup"
-log_file_name_batch_update = "fools-enterprise-log_Batch_Update"
-log_file_name_top_movers = "fools-enterprise-log-top-movers"
